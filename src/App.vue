6616bde26f72a472f6386b1c17a0108eaf752a14
lourencofelipe <felipe.pucsc@gmail.com>
2020-04-01 10:38:18 -0300

    Adding animation a list itens, using transition-group

--- src/App.vue
+++ src/App.vue
@@ -9,100 +9,65 @@
       </div>
 
       <div class="container">
-        <!-- <button class="btn btn-primary mb-3" @click="mostrar = !mostrar">Alternar</button> -->
-
+        <h3 class="font-weight-light">Tecnologias</h3>
+        <!-- Indicar para o transition-group qual elemento está sendo customizado tag="ul", por padrão ele definirá um span  -->
         <div class="form-group">
-          <label>Animações:</label>
-          <select class="form-control" v-model="alertaAtual">
-            <option value="info">Informação</option>
-            <option value="warning">Alerta</option>
-            <option value="success">Sucesso</option>
-          </select>
+          <input 
+          type="text" 
+          class="form-control"
+          placeholder="Insira um novo item e pressione Enter"
+          @keyup.enter="adicionar"
+          ref="input">
         </div>
-
-        <div class="form-group">
-          <label>Componente:</label>
-          <select class="form-control" v-model="componenteSelecionado">
-            <option value="AppHome">Home</option>
-            <option value="AppSobre">Sobre</option>
-          </select>
-        </div>
-
-        <transition :name="animacaoSelecionada" mode="out-in">
-          <component :is="componenteSelecionado"></component>
-        </transition>
+       
+        <transition-group tag="ul" class="list-group" name="list">
+          <li 
+          class="list-group-item" 
+          v-for="(tecnologia, indice) in tecnologias" 
+          :key="tecnologia">
+            <span>{{ tecnologia }}</span>
+            <button class="btn btn-danger btn-sm float-right"
+              @click="remover(indice)">
+               X
+            </button>
+          </li>
+        </transition-group>
       </div>
-
+       
     </div>
 </template>
 
 <script>
 
 export default {
-  components: {
-    AppHome: () => import('./components/Home.vue'),
-    AppSobre: () => import('./components/Sobre.vue')
-  },
   data(){
-    return {
-      mostrar: true,
-      animacaoSelecionada: 'fade',
-      alertaAtual: 'info',
-      componenteSelecionado: 'AppHome'
+    return{
+      tecnologias: [
+        'JavaScript',
+        'VueJs',
+        'Vuex',
+        'Vue Router'
+      ]
     }
   },
-  computed: {
-    classeDeAlerta(){
-      return {
-        alert: true,
-        [`alert-${this.alertaAtual}`] : true
+  methods: {
+    adicionar(event) {
+      const novoItem = event.target.value
+      if(novoItem) {
+        const indice = Math.floor(Math.random() * this.tecnologias.length)
+        this.tecnologias.splice(indice, 0, novoItem)
+        this.$refs.input.value = ''
       }
+    },
+    remover(indice) {
+      this.tecnologias.splice(indice, 1)
     }
   }
 }
 </script>
 
-<style>
-  body{
-    overflow: hidden;
-  }
-</style>
 <style scoped>
-  .slide-enter, .slide-leave-to {
-    opacity: 0;
-  }
-  .slide-enter-active {
-    animation: slide 0.7s;
-    transition: opacity 0.7s;
-  }
-  .slide-leave-active {
-    animation: slide 0.7s reverse;
-    transition: opacity 0.7s;
-  }
-  
-  @keyframes slide {
-    0% {
-      transform: translateX(-100px);
-    }
-    100% {
-      transform: translateX(0px);
-    }
-  }
-
-  /* Zoom */
-  .zoom-enter, .zoom-leave-to {
-    transform: scale(0);
-  }
-  .zoom-enter-active, .zoom-leave-active {
-    transition: transform 0.5s;
-  } 
-
-  /* Fade */
-  .fade-enter, .fade-leave-to {
-    opacity: 0;
-  }
-  .fade-enter-active, .fade-leave-active  {
-    transition: opacity 1s;
-  }
 
 </style>
+
+ 
